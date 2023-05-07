d1d683619b648b528029b73838f589b16d4d14c2
Mahmod Khayat <mahmoud.developer2@gmail.com>
2023-05-07 16:59:43 +0200

    Cube added
    added a cube just for testing

--- src/script.js
+++ src/script.js
@@ -1,51 +1,44 @@
 import "./style.css";
 import * as THREE from "three";
 
-let width=window.innerWidth;
-let height=window.innerHeight;
+let width = window.innerWidth;
+let height = window.innerHeight;
 
-const can=document.querySelector("#can");
+const can = document.querySelector("#can");
 
-can.setAttribute("width",width);
-can.setAttribute("height",height);
+can.setAttribute("width", width);
+can.setAttribute("height", height);
 
-const camera=new THREE.PerspectiveCamera(45,width/height,0.1,1000);
+const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
+camera.position.set(0, 0, 5);
 
-const scene=new THREE.Scene();
+const scene = new THREE.Scene();
 
-const renderer=new THREE.WebGL1Renderer({canvas:can});
+const renderer = new THREE.WebGL1Renderer({ canvas: can });
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(width,height);
-renderer.render(scene,camera);
+renderer.setSize(width, height);
 
-window.onresize=function(){
-                           width=window.innerWidth;
-                           height=window.innerHeight;
-                           can.setAttribute("width",width);
-                           can.setAttribute("height",height);
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-                           camera.aspect=width/height;
-                           camera.updateProjectionMatrix();
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 0, 10);
+scene.add(light);
 
-                           renderer.setSize(width,height);
-                           renderer.setPixelRatio(window.devicePixelRatio);
-                           };
+window.addEventListener("resize", () => {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  can.setAttribute("width", width);
+  can.setAttribute("height", height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
+});
 
-//Code
-
-
-
-//END Code
-
-const clock = new THREE.Clock();
-let oldElapsedTime=0;
-function animate(){
-        const elapsedTime=clock.getElapsedTime();
-        const deltaTime=elapsedTime-oldElapsedTime;
-        oldElapsedTime=elapsedTime;
-        
-        renderer.render(scene,camera);
-        
-        requestAnimationFrame(animate);
-        }
+function animate() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
 animate();
