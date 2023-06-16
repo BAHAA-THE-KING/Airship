fe033c8418a44d6e10143b8f2f3b5ab6d8537c87
MahmoudDe <mahmoud.developer2@gmail.com>
2023-06-16 15:54:40 +0200

    improve performance

--- src/scripts/city/building.js
+++ src/scripts/city/building.js
@@ -1,31 +1,33 @@
 import * as THREE from "three";
 
 export default class Building {
-  constructor(width, height, depth, textureUrl) {
+  constructor(width, height, depth, textureUrls) {
     this.width = width;
     this.height = height;
     this.depth = depth;
 
-    // Load texture
+    // Load textures
     const textureLoader = new THREE.TextureLoader();
-    const texture = textureLoader.load(textureUrl);
+    const texturePromises = textureUrls.map((url) => textureLoader.loadAsync(url));
 
-    // Create material with texture for the sides
-    const sideMaterial = new THREE.MeshPhongMaterial({ map: texture });
+    // Create materials with textures for the sides
+    Promise.all(texturePromises).then((textures) => {
+      const sideMaterials = textures.map((texture) => new THREE.MeshPhongMaterial({ map: texture }));
 
-    // Create material without texture for the top and bottom
-    const topBottomMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
+      // Create material without texture for the top and bottom
+      const topBottomMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
 
-    // Create mesh with geometry and materials
-    this.geometry = new THREE.BoxGeometry(width, height, depth);
-    this.mesh = new THREE.Mesh(this.geometry, [
-      sideMaterial, // right
-      sideMaterial, // left
-      topBottomMaterial, // top
-      topBottomMaterial, // bottom
-      sideMaterial, // front
-      sideMaterial, // back
-    ]);
+      // Create mesh with geometry and materials
+      this.geometry = new THREE.BoxGeometry(width, height, depth);
+      this.mesh = new THREE.Mesh(this.geometry, [
+        sideMaterials[0], // right
+        sideMaterials[1], // left
+        topBottomMaterial, // top
+        topBottomMaterial, // bottom
+        sideMaterials[2], // front
+        sideMaterials[3], // back
+      ]);
+    });
   }
 
   setPosition(x, y, z) {
