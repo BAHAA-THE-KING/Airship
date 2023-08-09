ab95018269e0305fe5eb91382924c20edc8b61b8
abdulrahman-kharzoum <aboodkh920@gmail.com>
2023-08-09 22:36:05 +0300

    add water and fps counter

--- src/script.js
+++ src/script.js
@@ -8,12 +8,17 @@ import makeGui from "./scripts/environment/GUI";
 import Blimp from "./scripts/models/Blimp";
 import PhysicsWorld from "./scripts/physics/PhysicsWorld";
 import { createClouds } from './scripts/Environment/clouds';
+import { Water } from 'three/examples/jsm/objects/Water.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+
+import { GUI } from 'lil-gui';
 
 //Initiate Renderer
 let width = window.innerWidth;
 let height = window.innerHeight;
 
-let sky, sun;
+let  sun,water;
+
 
 const can = document.querySelector("#can");
 
@@ -61,31 +66,95 @@ for (let i = 1; i < 8; i++) {
   textures[i] = textureLoader.load(`textures/clouds/${i}.png`);
   var clouds = createClouds(scene,textures[i],5);
 }
-
-// var texLoad = new THREE.TextureLoader(),
-//         addBlend = THREE.AdditiveBlending;
-// // Sky Texture
-// var skyMat = new THREE.MeshBasicMaterial({
-//         map: texLoad.load("textures/cloudMap.jpg"),
-//         side: THREE.BackSide,
-//         transparent: true,
-//         blending: addBlend,
-// });
-// var skySphere = new THREE.SphereGeometry(5000, 30, 30), skyMat;
-
-// var skyMesh = new THREE.Mesh(skySphere, skyMat);
-// scene.add(skyMesh);
-
-sky = new Sky();
-sky.scale.setScalar(45000);
-// sky.mesh = skyMesh;
-scene.add(sky);
-
 sun = new THREE.Vector3();
+
+// Water
+
+const waterGeometry = new THREE.PlaneGeometry( 2000,2000 );
+                                        
+water = new Water(
+waterGeometry,
+{
+textureWidth: 512,
+textureHeight: 512,
+waterNormals: new THREE.TextureLoader().load( 'textures/waternormals.jpg', function ( texture ) {
+
+texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+                } ),
+                sunDirection: new THREE.Vector3(),
+                sunColor: 0xffffff,
+                waterColor: 0x001e0f,
+                distortionScale: 3.7,
+                fog: scene.fog !== undefined
+        }
+);
+
+water.rotation.x = - Math.PI / 2;
+        water.position.y = 10;
+scene.add( water );
+
+// Skybox
+
+const sky = new Sky();
+sky.scale.setScalar( 10000 );
+scene.add( sky );
+
+const skyUniforms = sky.material.uniforms;
+
+skyUniforms[ 'turbidity' ].value = 10;
+skyUniforms[ 'rayleigh' ].value = 2;
+skyUniforms[ 'mieCoefficient' ].value = 0.005;
+skyUniforms[ 'mieDirectionalG' ].value = 0.8;
+
+const parameters = {
+        elevation: 2,
+        azimuth: 180
+};
+
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
+let renderTarget;
+
+function updateSun() {
+
+        const phi = THREE.MathUtils.degToRad( 90 - parameters.elevation );
+        const theta = THREE.MathUtils.degToRad( parameters.azimuth );
+
+        sun.setFromSphericalCoords( 1, phi, theta );
+
+        sky.material.uniforms[ 'sunPosition' ].value.copy( sun );
+        water.material.uniforms[ 'sunDirection' ].value.copy( sun ).normalize();
+
+        if ( renderTarget !== undefined ) renderTarget.dispose();
+
+        renderTarget = pmremGenerator.fromScene( sky );
+
+        scene.environment = renderTarget.texture;
+
+}
+
+updateSun();
+
+//
+
+const geometry = new THREE.BoxGeometry( 30, 30, 30 );
+const material = new THREE.MeshStandardMaterial( { roughness: 0 } );
+
+
+
+//
+
+
+
+const stats = new Stats();
+stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom)
 
 /**
  * GUI Variables
  */
+
+const gui = new GUI();
 const effectController = {
         turbidity: 10,
         rayleigh: 3,
@@ -111,9 +180,22 @@ function guiChanged() {
         uniforms['sunPosition'].value.copy(sun);
 
         renderer.toneMappingExposure = effectController.exposure;
+
+//         const waterUniforms = water.material.uniforms;
+
+// const folderWater = gui.addFolder( 'Water' );
+// folderWater.add( waterUniforms.distortionScale, 'value', 0, 8, 0.1 ).name( 'distortionScale' );
+// folderWater.add( waterUniforms.size, 'value', 0.1, 10, 0.1 ).name( 'size' );
+// folderWater.open();
+
         renderer.render(scene, camera);
 }
 
+
+
+		
+
+			
 const physicalVariables = {
         start: false,
         gravity: 9.8,
@@ -137,9 +219,6 @@ guiChanged();
 */
 createCity(scene);
 
-/**
- * Clouds
- */
 
 
 
@@ -159,15 +238,28 @@ const physicsWorld = new PhysicsWorld(blimp, physicalVariables, controls);
 const clock = new THREE.Clock();
 let oldElapsedTime = 0;
 function animate() {
+        stats.begin()
         const elapsedTime = clock.getElapsedTime();
         const deltaTime = elapsedTime - oldElapsedTime;
         oldElapsedTime = elapsedTime;
 
         if (physicalVariables.start) physicsWorld.update(deltaTime);
         controls.update();
+        render();
+        stats.update();
         clouds.rotation.y += 0.001;
         renderer.render(scene, camera);
-
+        stats.end()
         requestAnimationFrame(animate);
+}
+function render() {
+
+        const time = performance.now() * 0.001;
+
+  
+        water.material.uniforms[ 'time' ].value += 1.0 / 60.0;
+
+        renderer.render( scene, camera );
+
 }
 animate();
